df36bd1c6acff2da53612dfa856963866435cf03
TheSuperiorBpro <b3bpro456@gmail.com>
2023-04-08 00:02:52 +0700

    Fix DB và form đặt phòng tạm

---
+++ PBNL3/FormDatPhong.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBNL3
+{
+    public partial class FormDatPhong : Form
+    {
+        public FormDatPhong()
+        {
+            InitializeComponent();
+        }
+
+    private void LoadFormDatPhong()
+        {
+            guna2DateTimePicker1.TextAlign = HorizontalAlignment.Center;
+            
+        }
+
+    }
+}
