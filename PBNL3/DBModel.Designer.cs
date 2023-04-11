958514b81c29bafb60c8509c4ed8d0632ce3c2a6
TheSuperiorBpro <b3bpro456@gmail.com>
2023-04-11 12:29:58 +0700

    Fix

--- PBNL3/DBModel.Designer.cs
+++ PBNL3/DBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\thien\Source\Repos\UltraBpro\PBNL3\PBNL3\DBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Administrator\Desktop\New folder\PBNL3\PBNL3\DBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
